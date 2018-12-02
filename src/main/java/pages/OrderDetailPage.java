6f7f9708671717ac9026e4889366c3528f6042fa
Khanh Pham <Khanh Pham@DESKTOP-6UFQOA1>
2018-12-02 16:05:36 +0700

    TestCase07 verify status is pending

--- src/main/java/pages/OrderDetailPage.java
+++ src/main/java/pages/OrderDetailPage.java
@@ -1,15 +1,7 @@
 package pages;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import models.Order;
 import pagedom.OrderDetailPageDom;
 
 public class OrderDetailPage  extends BasePage {
--- src/test/java/test/TestCase07.java
+++ src/test/java/test/TestCase07.java
@@ -1,7 +1,11 @@
 package test;
 
+import java.util.List;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import models.Order;
 import pages.CustomerAccountPage;
 import pages.OrderDetailPage;
 
@@ -10,6 +14,10 @@ public class TestCase07 extends BaseTestCaseWithLogin {
 	public void VerifyPlacedOrder() {
 		CustomerAccountPage customerAccountPage = new CustomerAccountPage(driver);
 		customerAccountPage.getDocument().clickMyOrdersLink();
+
+		List<Order> orderList = customerAccountPage.getOrderList();
+		Assert.assertEquals(orderList.get(0).getStatus(), "Pending");
+
 		customerAccountPage.getDocument().clickViewOrderLink();
 
 		OrderDetailPage orderDetailPage = new OrderDetailPage(driver);
